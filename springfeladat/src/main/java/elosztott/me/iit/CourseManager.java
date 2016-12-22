5d0bc1af2d0715f55cc39ae740df4bbcaba1e0d9
DPKun <daniel.kun.92@gmail.com>
2016-12-22 21:40:27 +0100

    JSTL version
    JSTL Version of the project added

---
+++ springfeladat/src/main/java/elosztott/me/iit/CourseManager.java
@@ -0,0 +1,32 @@
+package elosztott.me.iit;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+@Service
+public class CourseManager implements Serializable{
+	/**
+	 * 
+	 */
+
+	private List<CourseModel> courses = new ArrayList<CourseModel>();
+	public void addCourse(CourseModel course){
+		courses.add(course);
+	}
+	public List<CourseModel> getCourses() {
+		return courses;
+	}
+	
+	public CourseModel getFirstCourse(){
+		if(!courses.isEmpty()){
+			return courses.get(0);
+		}
+		return new CourseModel("Unknown", -1, -1);
+	}
+	
+	public void clearCourses(){
+		courses.clear();
+	}
+}
