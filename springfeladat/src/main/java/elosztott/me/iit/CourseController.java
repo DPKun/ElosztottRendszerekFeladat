50ec2466f79a2b37c61527768faf9f8bbdc752cc
DPKun <daniel.kun.92@gmail.com>
2017-05-17 11:22:29 +0200

    HTML instead of JSP, Single Page Application

--- springfeladat/src/main/java/elosztott/me/iit/CourseController.java
+++ springfeladat/src/main/java/elosztott/me/iit/CourseController.java
@@ -12,55 +12,10 @@ public class CourseController {
 	@Autowired
 	CourseManager courseManager;
 	
-	
-	@RequestMapping(value={"","/"})
-	public ModelAndView index(){
-		ModelAndView model = new ModelAndView("index");
-		return model;
-		
-	}
-	
-	@RequestMapping(value="/welcome")
-	public ModelAndView welcome(){
-		//courseManager.clearCourses();
-		ModelAndView model = new ModelAndView("welcomepage");
-		//CourseModel course = new CourseModel("Analizis", 5, 1);
-		//courseManager.addCourse(course);
-		//CourseModel course2 = new CourseModel("Progalap", 5, 1);
-	//	CourseModel course3 = new CourseModel("Dimat", 5, 1);
-	//.addCourse(course2);
-	//	courseManager.addCourse(course3);
-		model.addObject("courses", courseManager.getCourses());
-		
-		return model;
-		
-	}
-	
-	@RequestMapping(value = "/addCourse", method=RequestMethod.GET)
-	public String newCourse(){
-		return "addCourse";
-	}
-	
-	
-	@RequestMapping(value = "/addCourse", method=RequestMethod.POST)
-	public ModelAndView newCoursePost(@ModelAttribute() CreateCourseDTO newCourseRequest){
-		if(newCourseRequest.getCourseName().trim().length()==0){
-			ModelAndView mav = new ModelAndView("addCourse");
-			mav.addObject("message", "Not valid courseName");
-			return mav;
-		}
-		else{
-			ModelAndView mav = new ModelAndView("redirect:/welcome");
-			courseManager.addCourse(new CourseModel(newCourseRequest.getCourseName(), newCourseRequest.getCourseCredit(), newCourseRequest.getPreferedSemester()));
-			mav.addObject("courses", courseManager.getCourses());
-			return mav;
-		}
-		}
-	
-	
-	@RequestMapping(value="/rest")
+
+	@RequestMapping(value="/")
 	public ModelAndView rest(){
-	        ModelAndView model = new ModelAndView("rest");
+	        ModelAndView model = new ModelAndView("static/html/rest.html");
 	        return model;
      
 	        }
--- springfeladat/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>task zer0 done!</h2>
-</body>
-</html>
